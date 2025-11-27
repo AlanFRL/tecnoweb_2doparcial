9462c120a19ac88e061533488885b47b38545ae5
AlanFRL <tengounsupercell@gmail.com>
2025-11-27 02:53:18 -0400

    excluir login de csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         // Excluir ruta de callback de PagoFacil de la verificación CSRF
         $middleware->validateCsrfTokens(except: [
             'payment/callback',
+            'login',
+            'login/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
